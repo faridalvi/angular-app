40df4d3c9de6eca1c370f3e5d1cb374f3eb643ba
faridtuf <ghulam.farid@tuf.edu.pk>
2022-11-18 15:02:48 +0500

    Add Service and Model in Login

---
+++ src/app/modules/auth/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Login } from './login.model';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  HTTP_OPTIONS = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': 'true'
+    }),
+  };
+  constructor(private http: HttpClient) { }
+  //Login
+  postLogin(data: Login) {
+    console.warn(data)
+    const body = new HttpParams()
+      .set('username', data.username)
+      .set('password', data.password)
+    return this.http.post<any>("", body, this.HTTP_OPTIONS)
+  }
+}
